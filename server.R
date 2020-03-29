66f3eb09b3b524263ee5ebc6dfce58128b78faac
Tanupat Boonchalermvichien <tanupat92@gmail.com>
2020-03-29 12:30:54 +0700

    update 29

--- server.R
+++ server.R
@@ -13,7 +13,11 @@ library(igraph)
 library(tidyverse)
 library(DT)
 
-averageCentrality <- function(graph, sel = c(1,2,3,4,5), topN = 10){
+modularList <- list('Whole graph' = 21, '1'=1, '2'=2, '3'=3, '4'=4, Chinese=5, '6'=6, Hollywood=7,'8'=8,'9'=9,'10'=10,'11'=11,'12'=12,'13'=13,
+                    '14'=14,'15'=15,'16'=16,'17'=17,'18'=18,'19'=19,'20'=20)
+eigenvectorList <- list('Whole graph' = 19, '1'=1, '2'=2, '3'=3, '4'=4, '5'=5, '6'=6, '7'=7,'8'=8,'9'=9,'10'=10,'11'=11,'12'=12,'13'=13,
+                        '14'=14,'15'=15,'16'=16,'17'=17,'18'=18)
+averageCentrality <- function(graph, sel = c(1,2,3,4,5), topN = 10, digits =2){
   #'input igraph type, output data.frame of nodes ranked by average centrality
   avLis <- list()
   for (i in sel){
@@ -63,19 +67,35 @@ averageCentrality <- function(graph, sel = c(1,2,3,4,5), topN = 10){
     df <- df[,c(2,1)]
     rankAv <- df %>% mutate(average = df[,2])
   }
-  
+  round_df <- function(x, digits) {
+    numeric_columns <- sapply(x, mode) == 'numeric'
+    x[numeric_columns] <-  round(x[numeric_columns], digits)
+    x
+  }
+  rankAv <- round_df(rankAv, digits)
   return(rankAv[1:topN,])
 }
 
 gUn <- readRDS('gUn.rds')
-denW <- cluster_fast_greedy(gUn, weights = E(gUn)$total) 
-commList <- list()
-for (i in 1:20){
-  commList[[i]] <- induced_subgraph(gUn, vids=denW$membership == i )
-}
 
 
-server <- function(input, output) {
+commList[[21]] <- gUn
+
+
+server <- function(input, output,session) {
+
+  reactive({
+    denW <- ifelse(input$community=="Greedy modularity maximization", 
+                             cluster_fast_greedy(gUn, weights = E(gUn)$total), 
+                             cluster_leading_eigen(gUn, weights = NULL))
+    commList <- list()
+    c <- ifelse(input$community=="Greedy modularity maximization", 20,18)
+    for (i in 1:c){
+    commList[[i]] <- induced_subgraph(gUn, vids=denW$membership == i )
+  }})
+  
+  
+  
   df <- reactive({averageCentrality(commList[[as.integer(input$class)]],sel = input$central, topN = as.integer(input$top))})
   dffull <- reactive({averageCentrality(commList[[as.integer(input$class)]], topN=gorder(commList[[as.integer(input$class)]]))})
   value <- reactive({df() %>% mutate(triple=average*20) %>% .[['triple']]})
@@ -95,9 +115,25 @@ server <- function(input, output) {
       visPhysics(stabilization = FALSE) %>% 
       visEdges(smooth = TRUE)
   })
-  
   output$table <- DT::renderDT({
     dffull()
+  })
+  
+  
+  observe({
+    x <- input$community
+    if (x == "Greedy modularity maximization"){
+      updateSelectInput(session, "class",
+                        label = paste("Select from 20 classes or the whole graph:"),
+                        choices = modularList,
+                        selected = 1)
+    } else {
+      updateSelectInput(session, "class",
+                        label = paste("Select from 18 classes or the whole graph:"),
+                        choices = eigenvectorList,
+                        selected = 1)
+    }
+        
   })
   
 }
