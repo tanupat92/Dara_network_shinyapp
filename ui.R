d39a4642f8ad1f2ed15cd7b529ad10442e81a006
Tanupat Boonchalermvichien <tanupat92@gmail.com>
2020-03-29 12:30:42 +0700

    update 29

--- ui.R
+++ ui.R
@@ -13,6 +13,11 @@ library(shinyWidgets)
 library(shinythemes)
 library(DT)
 
+modularList <- list('Whole graph' = 21, '1'=1, '2'=2, '3'=3, '4'=4, Chinese=5, '6'=6, Hollywood=7,'8'=8,'9'=9,'10'=10,'11'=11,'12'=12,'13'=13,
+                    '14'=14,'15'=15,'16'=16,'17'=17,'18'=18,'19'=19,'20'=20)
+eigenvectorList <- list('Whole graph' = 19, '1'=1, '2'=2, '3'=3, '4'=4, '5'=5, '6'=6, '7'=7,'8'=8,'9'=9,'10'=10,'11'=11,'12'=12,'13'=13,
+                    '14'=14,'15'=15,'16'=16,'17'=17,'18'=18)
+
 shinyUI(fluidPage(
   
   # Application title
@@ -20,9 +25,15 @@ shinyUI(fluidPage(
   theme = shinythemes::shinytheme('paper'),
   sidebarLayout(
     sidebarPanel(
-      pickerInput("class",
-                  "Choose modularity class (Greedy modularity maximization):",
-                  choices = 1:20),
+      radioButtons(
+        inputId = "community",
+        label = "Choose clustering methods:", 
+        choices = c("Greedy modularity maximization", "Leading eigenvector (unweighted)")
+      ),
+      selectInput("class",
+                  "Choose modularity class ():",
+                  choices = modularList,
+                  selected = 1),
       checkboxGroupInput(
         inputId = "central",
         label = "Choose centrality measures:", 
